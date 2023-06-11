a6d824357a5d048f8046294b6d49c48332caf3aa
georgiy <georgii_sidenko@mail.ru>
2023-06-11 18:00:56 +0300

    round number

--- lesson3/1.js
+++ lesson3/1.js
@@ -1,3 +1,3 @@
 let celsius = Number(prompt('Введите температуру в градусах цельсия:'));
 let fahrenheit = (9 / 5) * celsius + 32;
-alert('Цельсий: ' + (celsius) + ', Фаренгейт: ' + (fahrenheit));
+alert('Цельсий: ' + (celsius) + ', Фаренгейт: ' + (fahrenheit.toFixed(1)));
