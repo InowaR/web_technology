56a51ffd261d07289fb6ff82a50557408364df4c
georgiy <georgii_sidenko@mail.ru>
2023-06-16 21:57:43 +0300

    add array and a loop

--- lesson4/index.php
+++ lesson4/index.php
@@ -1,7 +1,28 @@
 <?php
-echo "hi";
 
 // https://www.w3schools.com/php/phptryit.asp?filename=tryphp_compiler
+
+$source_array = [
+    [
+    	'name' => 'John',
+        'description' => 'Builder. I am engaged in the construction of parks and recreation areas.',
+        'dateStart' => 'April 2020',
+        'dateEnd' => 'Current',
+    ],
+    [
+    	'name' => 'Jane',
+    	'description' => 'Medic. I work as a surgeon in a polyclinic.',
+        'dateStart' => 'December 2018',
+        'dateEnd' => 'August 2021',
+        
+    ],
+    [
+    	'name' => 'Ivan',
+        'description' => 'Driver. I have a personal car, I am engaged in cargo transportation.',
+        'dateStart' => 'May 2022',
+        'dateEnd' => 'October 2022',
+    ],
+];
 
 ?>
 
@@ -91,23 +112,21 @@ echo "hi";
     
       <div class="w3-container w3-card w3-white w3-margin-bottom">
         <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-suitcase fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Опыт работы</h2>
+        
+        <?php for($i = 0; $i < count($source_array); $i++): ?>
+        
         <div class="w3-container">
-          <h5 class="w3-opacity"><b>Front End Developer </b></h5>
-          <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Jan 2015 - <span class="w3-tag w3-teal w3-round">Current</span></h6>
-          <p>Lorem ipsum dolor sit amet. Praesentium magnam consectetur vel in deserunt aspernatur est reprehenderit sunt hic. Nulla tempora soluta ea et odio, unde doloremque repellendus iure, iste.</p>
+          <h5 class="w3-opacity"><b><?php echo $source_array[$i]['name']; ?></b></h5>
+          <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i><?php echo $source_array[$i]['dateStart']?> - <?php if($source_array[$i]['dateEnd'] == 'Current'): ?> <span class="w3-tag w3-teal w3-round">Current</span></h6>
+          <?php else: echo $source_array[$i]['dateEnd']; ?> 
+          </h6>
+          <?php endif; ?>
+          </h6>
+          <p><?php echo $source_array[$i]['description']; ?></p>
           <hr>
         </div>
-        <div class="w3-container">
-          <h5 class="w3-opacity"><b>Web Developer / something.com</b></h5>
-          <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Mar 2012 - Dec 2014</h6>
-          <p>Consectetur adipisicing elit. Praesentium magnam consectetur vel in deserunt aspernatur est reprehenderit sunt hic. Nulla tempora soluta ea et odio, unde doloremque repellendus iure, iste.</p>
-          <hr>
-        </div>
-        <div class="w3-container">
-          <h5 class="w3-opacity"><b>Graphic Designer / designsomething.com</b></h5>
-          <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Jun 2010 - Mar 2012</h6>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p><br>
-        </div>
+        <?php endfor; ?>
+  
       </div>
 
       <div class="w3-container w3-card w3-white">
